01ae5409a6301388857fa1e687704c60aeabe9a7
HyoungMin <hyoungqu23@gmail.com>
2024-01-05 11:03:30 +0900

    🎨 `routesProvider`를 활용하기 위한 `flutter_riverpod` 코드를 추가하고, `routesProvider`를 적용한다

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:apps_with_flutter/common/routes/routes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello Flutter'),
-          ),
-          body: Center(child: Text('Hello, World'))),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final routerConfig = ref.watch(routesProvider);
+
+    return MaterialApp.router(
+      title: '타이틀?',
+      routerConfig: routerConfig,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
