3bbd70503f3a9d6ee033cbb5148cc28e4af2fcd3
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-01-06 23:50:30 +0900

    🎨 Router 설정에 Plant App과 Stock App을 추가한다

--- lib/src/common/routes/routes.dart
+++ lib/src/common/routes/routes.dart
@@ -1,3 +1,6 @@
+import 'package:apps_with_flutter/src/features/home/views/home_screen.dart';
+import 'package:apps_with_flutter/src/features/plants_app/views/plants_app_screen.dart';
+import 'package:apps_with_flutter/src/features/stock_app/views/stock_app_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -9,9 +12,14 @@ class Routes {
     "path": '/',
   };
 
-  static const flutterAwesome = {
-    "name": 'Flutter Awesome',
-    "path": '/flutter-awesome:appId',
+  static const plantApp = {
+    "name": 'Plant App',
+    "path": '/plant-app',
+  };
+
+  static const stockApp = {
+    "name": 'Stock App',
+    "path": '/stock-app',
   };
 }
 
@@ -19,12 +27,17 @@ final appsWithFlutterRoutes = <RouteBase>[
   GoRoute(
     name: Routes.home['name'],
     path: Routes.home['path']!,
-    builder: (context, state) => const Text('HOME'),
+    builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
-    name: Routes.flutterAwesome['name'],
-    path: Routes.flutterAwesome['path']!,
-    builder: (context, state) => const Text('FLUTTER_AWESOME'),
+    name: Routes.plantApp['name'],
+    path: Routes.plantApp['path']!,
+    builder: (context, state) => const PlantAppScreen(),
+  ),
+  GoRoute(
+    name: Routes.stockApp['name'],
+    path: Routes.stockApp['path']!,
+    builder: (context, state) => const StockAppScreen(),
   ),
 ];
 
