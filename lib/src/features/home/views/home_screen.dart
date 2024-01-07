9f5260581bc13ebd062bca9dd65824ce04460978
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2024-01-07 23:41:42 +0900

    🚚 Wallet App으로 디렉토리와 라우팅 구조를 변경한다

--- lib/src/features/home/views/home_screen.dart
+++ lib/src/features/home/views/home_screen.dart
@@ -1,12 +1,12 @@
 import 'package:apps_with_flutter/src/common/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:apps_with_flutter/src/features/plants_app/views/plants_app_screen.dart';
 import 'package:go_router/go_router.dart';
 
-List<Object> apps = [
-  {'id': 'plantApp', 'widget': PlantAppScreen},
-  {'id': 'app2', 'widget': PlantAppScreen},
-];
+// List<Object> apps = [
+//   {'id': 'Plant App'},
+//   {'id': 'Stock App'},
+//   {'id': 'app2'},
+// ];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -29,19 +29,29 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               children: [
                 ElevatedButton(
-                    onPressed: () {
-                      context.pushNamed(
-                        Routes.plantApp['name']!,
-                      );
-                    },
-                    child: const Text('Plant App')),
+                  onPressed: () {
+                    context.pushNamed(
+                      Routes.plantApp['name']!,
+                    );
+                  },
+                  child: const Text('Plant App'),
+                ),
                 ElevatedButton(
-                    onPressed: () {
-                      context.pushNamed(
-                        Routes.stockApp['name']!,
-                      );
-                    },
-                    child: const Text('Stock App')),
+                  onPressed: () {
+                    context.pushNamed(
+                      Routes.walletApp['name']!,
+                    );
+                  },
+                  child: const Text('Stock App'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.pushNamed(
+                      Routes.travelApp['name']!,
+                    );
+                  },
+                  child: const Text('Travel App'),
+                ),
               ],
             ),
           ),
