c9df072227e9bea574a03a98e0e12a87add5505e
HyoungMin <hyoungqu23@gmail.com>
2024-01-08 09:15:47 +0900

    💄 Home의 List UI를 ListView 위젯으로 변경한다

--- lib/src/features/home/views/home_screen.dart
+++ lib/src/features/home/views/home_screen.dart
@@ -1,12 +1,10 @@
-import 'package:apps_with_flutter/src/common/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-// List<Object> apps = [
-//   {'id': 'Plant App'},
-//   {'id': 'Stock App'},
-//   {'id': 'app2'},
-// ];
+List<Map<String, String>> apps = [
+  {'id': 'Wallet App', 'description': 'Only UI App'},
+  {'id': 'Plant App', 'description': 'Only UI App'},
+];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,62 +12,84 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'App UIs',
-            style: TextStyle(
-              color: Colors.indigo,
-              fontSize: 24,
-              fontWeight: FontWeight.w900,
-            ),
+      backgroundColor: Colors.indigo.shade50,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo.shade50,
+        title: const Text(
+          'Exercise Flutter UIs',
+          style: TextStyle(
+            color: Colors.indigo,
+            fontSize: 28,
+            fontWeight: FontWeight.w900,
           ),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.pushNamed(
-                      Routes.plantApp['name']!,
-                    );
-                  },
-                  child: const Text('Plant App'),
+      ),
+      // body: Center(
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: [
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             context.pushNamed(
+      //               Routes.plantApp['name']!,
+      //             );
+      //           },
+      //           child: const Text('Plant App'),
+      //         ),
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             context.pushNamed(
+      //               Routes.walletApp['name']!,
+      //             );
+      //           },
+      //           child: const Text('Stock App'),
+      //         ),
+      //         ElevatedButton(
+      //           onPressed: () {
+      //             context.pushNamed(
+      //               Routes.travelApp['name']!,
+      //             );
+      //           },
+      //           child: const Text('Travel App'),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: ListView.separated(
+        itemCount: apps.length,
+        separatorBuilder: (context, index) => const Divider(height: 0),
+        itemBuilder: (context, index) {
+          final String appName = apps[index]['id']!;
+          final String appDescription = apps[index]['description']!;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ListTile(
+              onTap: () {
+                context.pushNamed(appName);
+              },
+              contentPadding: EdgeInsets.zero,
+              leading: const Icon(
+                Icons.app_shortcut_outlined,
+                size: 36,
+              ),
+              title: Text(
+                appName,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    context.pushNamed(
-                      Routes.walletApp['name']!,
-                    );
-                  },
-                  child: const Text('Stock App'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    context.pushNamed(
-                      Routes.travelApp['name']!,
-                    );
-                  },
-                  child: const Text('Travel App'),
-                ),
-              ],
+              ),
+              subtitle: Text(appDescription),
+              trailing: const Icon(
+                Icons.arrow_circle_right_outlined,
+                size: 24,
+              ),
             ),
-          ),
-        )
-        // body: ListView.separated(
-        //   itemCount: apps.length,
-        //   separatorBuilder: (context, index) => const Divider(height: 0),
-        //   itemBuilder: (context, index) => const Padding(
-        //     padding: EdgeInsets.symmetric(horizontal: 16),
-        //     child: ListTile(
-        //       contentPadding: EdgeInsets.zero,
-        //       leading: CircleAvatar(),
-        //       title: Text('title'),
-        //       subtitle: Text('sub'),
-        //       trailing: Icon(Icons.paypal),
-        //     ),
-        //   ),
-        // ),
-        );
+          );
+        },
+      ),
+    );
   }
 }
