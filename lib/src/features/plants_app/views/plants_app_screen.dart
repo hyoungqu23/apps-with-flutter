81a1b7725462e3751286b1f2dbde2b838a0f1379
HyoungMin <hyoungqu23@gmail.com>
2024-01-09 17:21:49 +0900

    ✨ plant_app_screen의 기본 구조를 추가한다

---
+++ lib/src/features/plants_app/views/plants_app_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+class PlantAppScreen extends StatelessWidget {
+  const PlantAppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
